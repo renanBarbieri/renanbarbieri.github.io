9a621a8c84d9694089aa4fa6545c070f65d0bfba
Renan Barbieri <renan.hbarbieri@gmail.com>
2016-07-04 18:45:27 -0300

    setorizando areas do site e colocando mais informações pessoais

--- assets/js/main.js
+++ assets/js/main.js
@@ -55,11 +55,11 @@ jQuery(document).ready(function($) {
     );
     
     /* Github Calendar - https://github.com/IonicaBizau/github-calendar */
-    GitHubCalendar("#github-graph", "IonicaBizau");
+    // GitHubCalendar("#github-graph", "IonicaBizau");
     
     
     /* Github Activity Feed - https://github.com/caseyscarborough/github-activity */
-    GitHubActivity.feed({ username: "caseyscarborough", selector: "#ghfeed" });
+    // GitHubActivity.feed({ username: "caseyscarborough", selector: "#ghfeed" });
 
 
 });
